4f2c932986fa6adf94f4a9f37be74e68a70bbd18
Chris Betz <chris.betz@carecaminnovations.com>
2016-01-12 15:44:30 -0500

    ditching mongodb for postgres, and speed!

--- main.go
+++ main.go
@@ -2,22 +2,13 @@ package main
 
 import (
 	"bufio"
-	"compress/bzip2"
-	"io"
-	"io/ioutil"
+	"database/sql"
 	"log"
-	"net/http"
 	"os"
-	"strconv"
 	"strings"
-	"sync"
 	"time"
 
-	"golang.org/x/text/encoding/charmap"
-	"golang.org/x/text/transform"
-
-	"gopkg.in/mgo.v2"
-	"gopkg.in/mgo.v2/bson"
+	"github.com/lib/pq"
 )
 
 type User struct {
@@ -34,7 +25,7 @@ type Data struct {
 
 func main() {
 
-	output, err := os.Create("daily_user_summary.txt.bz2")
+	/*output, err := os.Create("daily_user_summary.txt.bz2")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -72,7 +63,7 @@ func main() {
 	if err != nil {
 		println("error writing file: ")
 		log.Fatal(err)
-	}
+	} */
 
 	file, err := os.Open("daily_user_summary.txt")
 	if err != nil {
@@ -84,90 +75,56 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
 
-	session, err := mgo.Dial("localhost")
-	if err != nil {
-		println("error dialing DB")
-		log.Fatal(err)
-	}
-	defer session.Close()
-
 	// Skip the date line.
 	scanner.Scan()
 
 	// Skip the column header line.
 	scanner.Scan()
 
-	for {
-		// Create a wait group to manage the goroutines.
-		var waitGroup sync.WaitGroup
+	start := time.Now()
 
-		start := time.Now()
-
-		// Perform 10 concurrent queries against the database.
-		waitGroup.Add(10)
-		for query := 0; query < 10; query++ {
-			if scanner.Scan() {
-				go updateUserData(query, &waitGroup, session, scanner.Text())
-			} else {
-				break
-			}
-		}
-
-		// Wait for all the queries to complete.
-		waitGroup.Wait()
-
-		elapsed := time.Since(start)
-		log.Printf("Processed in %s", elapsed)
-
-		if scanner.Scan() == false {
-			break
-		}
-	}
-}
-
-func updateUserData(query int, waitGroup *sync.WaitGroup, mongoSession *mgo.Session, line string) {
-	entry := strings.Split(line, "\t")
-
-	score, err := strconv.ParseUint(entry[1], 10, 64)
+	// lazily open db (doesn't truly open until first request)
+	db, err := sql.Open("postgres", "host=localhost dbname=crease sslmode=disable")
 	if err != nil {
-		println("error parsing score")
+		log.Fatal(err)
 	}
 
-	wu, err := strconv.ParseUint(entry[2], 10, 64)
+	txn, err := db.Begin()
 	if err != nil {
-		println("error parsing wu")
+		log.Fatal(err)
 	}
 
-	team, err := strconv.ParseUint(entry[3], 10, 64)
+	stmt, err := txn.Prepare(pq.CopyIn("users", "name"))
 	if err != nil {
-		println("error parsing team:" + err.Error())
+		log.Fatal(err)
 	}
 
-	updateDate := time.Now()
-	data := Data{Score: score, WorkUnits: wu, Date: updateDate}
-	user := User{Name: entry[0], Team: team}
+	for scanner.Scan() {
+		entry := strings.Split(scanner.Text(), "\t")
 
-	// Decrement the wait group count so the program knows this
-	// has been completed once the goroutine exits.
-	defer waitGroup.Done()
+		user := User{Name: entry[0]}
 
-	// Request a socket connection from the session to process our query.
-	// Close the session when the goroutine exits and put the connection back
-	// into the pool.
-	sessionCopy := mongoSession.Copy()
-	defer sessionCopy.Close()
-
-	c := sessionCopy.DB("crease").C("users")
-
-	result := new(User)
-	err = c.Find(bson.M{"name": user.Name}).One(&result)
-	if err != nil {
-		err = c.Insert(user)
+		_, err = stmt.Exec(user.Name)
 		if err != nil {
 			log.Fatal(err)
 		}
-		*result = user
 	}
 
-	c.Update(result, bson.M{"$push": bson.M{"data": data}})
+	_, err = stmt.Exec()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = stmt.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = txn.Commit()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	elapsed := time.Since(start)
+	log.Printf("Processed in %s", elapsed)
 }
